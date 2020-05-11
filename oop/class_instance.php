4fa90963e4400e8496c61b9788d0dd3b0ca7230f
Gekif <dzulfikar.maulana@gmail.com>
2020-05-11 09:57:47 +0700

    02 OOP Fundamentals: 010 Instantiating a Class

---
+++ oop/class_instance.php
@@ -0,0 +1,20 @@
+<?php
+
+class Cars
+{
+    function greeting()
+    {
+        echo 'Hello Student';
+    }
+}
+
+$bww = new Cars();
+$mercedes = new Cars();
+
+$bww->greeting();
+echo '<br>';
+$mercedes->greeting();
+
+
+
+
