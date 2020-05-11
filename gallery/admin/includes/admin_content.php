56e5d8ac3cf5390deb5ada79e412607cbde06e50
Gekif <dzulfikar.maulana@gmail.com>
2020-05-11 21:27:53 +0700

    04 Database Class: 033 Testing Query Method

--- gallery/admin/includes/admin_content.php
+++ gallery/admin/includes/admin_content.php
@@ -8,11 +8,17 @@
                 <small>Subheading</small>
             </h1>
 
+
             <?php
-            if ($database->connection) {
-                echo 'true';
-            }
+            $sql = "SELECT * FROM users WHERE id = 1";
+
+            $result = $database->query($sql);
+
+            $user_found = mysqli_fetch_array($result);
+
+            echo "<h2>{$user_found['username']}</h2>";
             ?>
+
 
             <ol class="breadcrumb">
                 <li>
