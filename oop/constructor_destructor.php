18f08cd4382cde307f8d2bb281873e8e408d47d7
Gekif <dzulfikar.maulana@gmail.com>
2020-05-11 19:49:42 +0700

    02 OOP Fundamentals: 017 Constructors and Destructors

---
+++ oop/constructor_destructor.php
@@ -0,0 +1,29 @@
+<?php
+
+class Cars
+{
+    public $wheel_count = 4;
+    static $door_count = 4;
+
+    function __construct()
+    {
+//        echo $this->wheel_count;
+        echo self::$door_count++;
+    }
+
+    function __destruct()
+    {
+        echo self::$door_count--;
+    }
+
+//    function details()
+//    {
+//        echo $this->wheel_count;
+//    }
+}
+
+// With Constructor can call the class it self automatically (magic method)
+$bmw = new Cars();
+$mercedes = new Cars();
+
+//$bmw->details()
