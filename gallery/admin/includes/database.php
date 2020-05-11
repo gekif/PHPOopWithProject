b45d6d744e841883ff3b27191cb8c066a63abc31
Gekif <dzulfikar.maulana@gmail.com>
2020-05-11 21:23:19 +0700

    04 Database Class: 032 Database Class Helper Methods

--- gallery/admin/includes/database.php
+++ gallery/admin/includes/database.php
@@ -26,11 +26,23 @@ class Database
     {
         $result = mysqli_query($this->connection, $sql);
 
+        return $result;
+    }
+
+
+    private function confirm_query($result)
+    {
         if (!$result) {
             die('Query Failed');
         }
+    }
 
-        return $result;
+
+    public function escape_string($string)
+    {
+        $escaped_string = mysqli_real_escape_string($this->connection, $string);
+
+        return $escaped_string;
     }
 }
 
