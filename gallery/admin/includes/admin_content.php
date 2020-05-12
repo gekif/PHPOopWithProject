bc971b7cbbcee7b18a10afe2ef4bb02b30add7df
Gekif <dzulfikar.maulana@gmail.com>
2020-05-12 23:01:17 +0700

    05 The User Class: 047 Using our Instantiation Method to find all users

--- gallery/admin/includes/admin_content.php
+++ gallery/admin/includes/admin_content.php
@@ -10,19 +10,12 @@
 
 
             <?php
-//            $result_set = User::find_all_users();
-//
-//            while ($row = mysqli_fetch_array($result_set)) {
-//                echo $row['username'] . '<br>';
-//            }
+            $users = User::find_all_users();
 
-//            $found_user = User::find_user_by_id(2);
-//
-//            $user = User::instantiation($found_user);
-//
-//            echo $user->username;
-//
-//            echo '<br>';
+            foreach ($users as $user) {
+                echo $user->username;
+                echo '<br>';
+            }
             ?>
 
 
