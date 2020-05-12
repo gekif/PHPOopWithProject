9add99a31d007e9134f5e6cc8222806f3a609110
Gekif <dzulfikar.maulana@gmail.com>
2020-05-12 23:06:45 +0700

    05 The User Class: 048 Using our Instantiation Method to find 1 User

--- gallery/admin/includes/admin_content.php
+++ gallery/admin/includes/admin_content.php
@@ -10,12 +10,16 @@
 
 
             <?php
-            $users = User::find_all_users();
+//            $users = User::find_all_users();
+//
+//            foreach ($users as $user) {
+//                echo $user->username;
+//                echo '<br>';
+//            }
 
-            foreach ($users as $user) {
-                echo $user->username;
-                echo '<br>';
-            }
+            $found_user = User::find_user_by_id(2);
+
+            echo $found_user->username;
             ?>
 
 
--- gallery/admin/includes/user.php
+++ gallery/admin/includes/user.php
@@ -17,11 +17,18 @@ class User
 
     public static function find_user_by_id($user_id)
     {
-        $result_set =  self::find_this_query("SELECT * FROM users WHERE id  = $user_id LIMIT 1");
+        $the_result_array =  self::find_this_query("SELECT * FROM users WHERE id  = $user_id LIMIT 1");
 
-        $found_user = mysqli_fetch_array($result_set);
+        return !empty($the_result_array) ? array_shift($the_result_array) : false;
 
-        return $found_user;
+//        if (!empty($the_result_array)) {
+//            $first_item = array_shift($the_result_array);
+//
+//            return $first_item;
+//
+//        } else {
+//            return false;
+//        }
     }
 
 
