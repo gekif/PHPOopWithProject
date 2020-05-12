1aaa4a4bca3c62953d132151c7802a139738aed7
Gekif <dzulfikar.maulana@gmail.com>
2020-05-12 22:14:43 +0700

    05 The User Class: 042 Auto Instantiation Method

--- gallery/admin/includes/user.php
+++ gallery/admin/includes/user.php
@@ -11,26 +11,12 @@ class User
 
     public static function find_all_users()
     {
-//        global $database;
-//
-//        $result_set = $database->query("SELECT * FROM users");
-//
-//        return $result_set;
-
         return self::find_this_query("SELECT * FROM users");
     }
 
 
     public static function find_user_by_id($user_id)
     {
-//        global $database;
-//
-//        $result_set = $database->query("SELECT * FROM users WHERE id  = $user_id LIMIT 1");
-//
-//        $found_user = mysqli_fetch_array($result_set);
-//
-//        return $found_user;
-
         $result_set =  self::find_this_query("SELECT * FROM users WHERE id  = $user_id LIMIT 1");
 
         $found_user = mysqli_fetch_array($result_set);
@@ -46,5 +32,20 @@ class User
         $result_set = $database->query($sql);
 
         return $result_set;
+    }
+
+
+    private static function instantiation()
+    {
+        $the_object = new self;
+
+        $the_object->id = $found_user['id'];
+        $the_object->username = $found_user['username'];
+        $the_object->password = $found_user['password'];
+        $the_object->first_name = $found_user['first_name'];
+        $the_object->last_name = $found_user['last_name'];
+
+        return $the_object;
+
     }
 }
