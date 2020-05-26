2d471d9528129ca3bd32cc6ad69682602d20d321
Gekif <dzulfikar.maulana@gmail.com>
2020-05-26 17:16:49 +0700

    06 The Session Class: 055 The Login Method

--- gallery/admin/includes/session.php
+++ gallery/admin/includes/session.php
@@ -2,12 +2,27 @@
 
 class Session
 {
-    private $signed_in;
+    private $signed_in = false;
     public $user_id;
 
     function __construct()
     {
         session_start();
+
+        $this->check_the_login();
+    }
+
+    public function is_signed_in()
+    {
+        return $this->$signed_in;
+    }
+
+    public function login($user)
+    {
+        if ($user) {
+            $this->user_id = $_SESSION['used_id'] = $user->id;
+            $this->signed_in = true;
+        }
     }
 
     private function check_the_login()
