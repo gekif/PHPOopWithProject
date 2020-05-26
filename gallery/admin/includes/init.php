f125813b08b061faaaa309635a9488de1707e2d8
Gekif <dzulfikar.maulana@gmail.com>
2020-05-26 17:07:06 +0700

    06 The Session Class: 053 Starting Sessions

--- gallery/admin/includes/init.php
+++ gallery/admin/includes/init.php
@@ -1,6 +1,7 @@
 <?php
 
-include('functions.php');
-include('dbconfig.php');
-include('database.php');
-//include('user.php');
+require_once('functions.php');
+require_once('dbconfig.php');
+require_once('database.php');
+require_once('user.php');
+require_once('session.php');
